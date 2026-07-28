927455d828d4eba67431690ad965d1e8dc8760be
vsemenuha-lgtm <vsemenuha@gmail.com>
2026-07-28 23:17:37 +0300

    замена на HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import ProjectDetails from './pages/ProjectDetails';
 
@@ -31,7 +31,7 @@ function ScrollObserver() {
 
 function App() {
   return (
-    <Router basename="/LUXEDESIGN">
+    <Router>
       <ScrollObserver />
       <Routes>
         <Route path="/" element={<Home />} />
